cc4ebf75712227a143d5f8b9e1d409715b02f248
Andreas Niedermair <andreas@niedermair.name>
2016-04-08 14:14:39 +0200

    added basic installation

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dashlane'
+$installerType = 'exe'
+$silentArgs = '/SD'
+$url = 'https://d3qm0vl2sdkrc.cloudfront.net/alpha/Dashlane_Launcher_bchrome-1458725556.exe'
+$checksum = '709ac46bb1f6b2c8960a12db403ddaf75c5ff1a7'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
