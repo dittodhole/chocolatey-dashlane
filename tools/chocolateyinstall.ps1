990942999dae51c9b8132631fec217c5214c9c8d
Andreas Niedermair <andreas@niedermair.name>
2016-04-08 14:19:52 +0200

    fixed exitcode of installer

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $silentArgs = '/SD'
 $url = 'https://d3qm0vl2sdkrc.cloudfront.net/alpha/Dashlane_Launcher_bchrome-1458725556.exe'
 $checksum = '709ac46bb1f6b2c8960a12db403ddaf75c5ff1a7'
 $checksumType = 'sha1'
-$validExitCodes = @(0)
+$validExitCodes = @(0,1223)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
